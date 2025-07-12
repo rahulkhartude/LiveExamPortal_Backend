645e073b1ea6379601e286cbf5258d20fb0da9fd
rahulkhartude <rahulkhartude31@gmail.com>
2025-07-12 23:30:01 +0530

    Initial commit

--- server/models/Question.js
+++ server/models/Question.js
@@ -1,9 +1,26 @@
+// const mongoose = require('mongoose');
+
+// const questionSchema = new mongoose.Schema({
+//   section: String, // e.g., 'React'
+//   text: String,
+//   options: [String],
+//   correctAnswer: Number // index of correct option
+// });
+// module.exports = mongoose.model('Question', questionSchema);
+
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
-  section: String, // e.g., 'React'
-  text: String,
-  options: [String],
-  correctAnswer: Number // index of correct option
+  section: String,           // e.g., 'Reasoning'
+  text: String,              // question text
+  options: [String],         // list of options
+  type: {                    // single, multiple, truefalse
+    type: String,
+    enum: ['single', 'multiple', 'truefalse'],
+    required: true
+  },
+  correctAnswer: mongoose.Schema.Types.Mixed
+  // can be Number for single/truefalse, or [Number] for multiple
 });
+
 module.exports = mongoose.model('Question', questionSchema);
